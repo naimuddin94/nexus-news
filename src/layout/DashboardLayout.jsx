5eed10893dcfa20f47528f605fc0ba37185f1bf3
naimuddin94 <developernaimuddin@gmail.com>
2023-11-29 16:21:44 +0600

    dashboard responsive

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -8,8 +8,8 @@ const DashboardLayout = () => {
     <>
       <Navbar />
       <hr />
-      <div className="grid grid-cols-12">
-        <div className="min-h-screen bg-secondary col-span-2 p-4">
+      <div className="md:grid grid-cols-12">
+        <div className="h-full bg-secondary col-span-2 p-4">
           <Sidebar />
         </div>
         <div className="min-h-screen bg-base-100 col-span-10 p-5">
