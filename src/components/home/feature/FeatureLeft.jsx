e8ddff7acca58f1b564442364c693fd917b56a5b
naimuddin94 <developernaimuddin@gmail.com>
2023-12-13 00:10:27 +0600

    scrolling effect added

--- src/components/home/feature/FeatureLeft.jsx
+++ src/components/home/feature/FeatureLeft.jsx
@@ -4,6 +4,7 @@ import FeatureCard from "./FeatureCard";
 import Marquee from "react-fast-marquee";
 import FeatureNewsCard from "./FeatureNewsCard";
 import HeadingCard from "./HeadingCard";
+import styles from "./scrollbar.module.css";
 
 const FeatureLeft = () => {
   const { articles } = useArticles();
@@ -21,14 +22,16 @@ const FeatureLeft = () => {
         ))}
       </div>
       <hr />
-      <div className="grid grid-cols-1 md:grid-cols-12 pt-3">
+      <div className="grid grid-cols-1 md:grid-cols-12 pt-3 ">
         <div className="col-span-4">
           <HighLightHeading text="World" />
           {articles?.map((article) => (
             <HeadingCard key={article._id} article={article} />
           ))}
         </div>
-        <div className="col-span-8 grid gap-3 grid-cols-1 pl-2">
+        <div
+          className={`col-span-8 grid gap-3 grid-cols-1 pl-2 max-h-[1700px] overflow-y-scroll ${styles.customScrollbar}`}
+        >
           {articles?.map((article) => (
             <FeatureNewsCard key={article._id} article={article} />
           ))}
--- src/components/home/feature/FeatureRight.jsx
+++ src/components/home/feature/FeatureRight.jsx
@@ -1,13 +1,16 @@
 import useArticles from "../../../hooks/useArticles";
 import HighLightHeading from "../../utility/HighLightHeading";
 import SportsCard from "./SportsCard";
+import styles from  "./scrollbar.module.css";
 
 const FeatureRight = () => {
   const { articles } = useArticles();
   return (
     <div className="">
       <HighLightHeading text="Sports" />
-      <div className="">
+      <div
+        className={`max-h-[1960px] overflow-y-scroll ${styles.customScrollbar}`}
+      >
         {articles?.slice(0, 5).map((article) => (
           <SportsCard key={article._id} article={article} />
         ))}
