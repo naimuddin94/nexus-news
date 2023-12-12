9a30c095e0147555647112db7216c7b21b1bf643
naimuddin94 <developernaimuddin@gmail.com>
2023-12-13 00:51:50 +0600

    subscription features modal open

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,26 @@
-import { Header,Category, Hero,Feature } from "../../components/home";
-
+import { useEffect, useState } from "react";
+import { Header, Category, Hero, Feature } from "../../components/home";
+import Advertizement from "./Advertizement";
+import useAuthInfo from "../../hooks/useAuthInfo";
 
 const Home = () => {
+  const [showModal, setShowModal] = useState(false);
+  const { premiumUser, role } = useAuthInfo();
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (!premiumUser && role === "normal") {
+        setShowModal(true);
+      }
+    }, 5000);
+  }, [premiumUser, role]);
   return (
     <div>
       <Header />
       <Category />
       <Hero />
       <Feature />
+      <Advertizement showModel={showModal} />
     </div>
   );
 };
